7e6a72d752804676d38d4d8f705203d384ba9c98
Kenny <kenny_oliver@icloud.com>
2021-03-27 11:19:24 +0000

    [+] IT’S WORKING!!! Thanks to https://enlight.nyc/projects/weather/

---
+++ script.js
@@ -0,0 +1,35 @@
+function getWeather() {
+  let temperature = document.getElementById("temperature");
+  let description = document.getElementById("description");
+  let location = document.getElementById("location");
+
+  let api = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "4a8e56d76aab3920b24af328c175da5b";
+
+  location.innerHTML = "Locating...";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    let url = `${api}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        let temp = data.main.temp;
+        temperature.innerHTML = temp + "° C";
+        location.innerHTML = data.name + " (" + latitude + "°, " + longitude + "°)";
+        description.innerHTML = data.weather[0].main;
+      });
+  }
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
+}
+
+getWeather();
