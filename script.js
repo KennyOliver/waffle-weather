65a11d14ea2e00282dbd86cd93e494d49a8c7882
Kenny <kenny_oliver@icloud.com>
2021-03-27 11:43:53 +0000

    [+] Added current weather icon

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function getWeather() {
   let description = document.getElementById("description");
   let location = document.getElementById("location");
   let geolocation = document.getElementById("geolocation");
+  let weather_img = document.getElementById("weather-img");
 
   let api = "https://api.openweathermap.org/data/2.5/weather";
   let apiKey = "4a8e56d76aab3920b24af328c175da5b";
@@ -25,6 +26,8 @@ function getWeather() {
         temperature.innerHTML = Math.round(data.main.temp) + "°C";
         description.innerHTML = data.weather[0].main;
         geolocation.innerHTML = `(${latitude}°, ${longitude}°)`;
+        
+        weather_img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       });
   }
 
